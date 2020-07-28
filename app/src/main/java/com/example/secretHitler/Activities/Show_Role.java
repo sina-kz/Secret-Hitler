3ac655d21d98ebaa905424035e1b90d99faf3d83
Sepehr Feli <sepehr.feli.1377@gmail.com>
2020-07-28 23:41:56 +0430

    anim debugged

--- app/src/main/java/com/example/secretHitler/Activities/Show_Role.java
+++ app/src/main/java/com/example/secretHitler/Activities/Show_Role.java
@@ -3,29 +3,20 @@ package com.example.secretHitler.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.secretHitler.Enums.Team;
 import com.example.secretHitler.Fragments.Show_Fascist_Fragment;
 import com.example.secretHitler.Fragments.Show_Hitler_Fragment;
 import com.example.secretHitler.Fragments.Show_Liberal_Fragment;
-import com.example.secretHitler.Models.GameMethods;
 import com.example.secretHitler.Models.Player;
 import com.example.secretHitler.R;
 import com.google.gson.Gson;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Show_Role extends AppCompatActivity {
 
@@ -34,8 +25,6 @@ public class Show_Role extends AppCompatActivity {
     private Fragment fragment;
     private int index;
     ArrayList<Player> players;
-    Player player;
-    ArrayList<Player> teammates;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -53,9 +42,7 @@ public class Show_Role extends AppCompatActivity {
         final Intent boardGameIntent = new Intent(getBaseContext(), BoardGameActivity.class);
         checkTeam(players);
 
-        player = players.get(index);
-        teammates = GameMethods.teammatesToBeShown(players, player);
-        textView.setText(player.getName() + "\n\n" + player.teammatesString(teammates));
+        textView.setText(players.get(index).getName());
         getSupportFragmentManager().beginTransaction()
                 .setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_right_to_left
                         , R.anim.enter_left_to_right, R.anim.exit_left_to_right)
@@ -71,9 +58,12 @@ public class Show_Role extends AppCompatActivity {
                 if (index != 0) {
                     index--;
                     checkTeam(players);
-                    textView.setText(player.getName() + "\n\n" + player.teammatesString(teammates));
-                } else {
-                    previous.setVisibility(View.INVISIBLE);
+                    textView.setText(players.get(index).getName());
+                    getSupportFragmentManager().beginTransaction()
+                            .setCustomAnimations(R.anim.enter_left_to_right, R.anim.exit_left_to_right)
+                            .replace(R.id.container, fragment)
+                            .addToBackStack(null)
+                            .commit();
                 }
             }
         });
@@ -83,8 +73,13 @@ public class Show_Role extends AppCompatActivity {
                 if (index != players.size() - 1) {
                     index++;
                     checkTeam(players);
-                    textView.setText(player.getName() + "\n\n" + player.teammatesString(teammates));
+                    textView.setText(players.get(index).getName());
                     previous.setVisibility(View.VISIBLE);
+                    getSupportFragmentManager().beginTransaction()
+                            .setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_right_to_left)
+                            .replace(R.id.container, fragment)
+                            .addToBackStack(null)
+                            .commit();
                 } else {
                     SharedPreferences sharedPreferences = getSharedPreferences("my shared", MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -110,11 +105,6 @@ public class Show_Role extends AppCompatActivity {
                 fragment = new Show_Fascist_Fragment();
             }
         }
-        getSupportFragmentManager().beginTransaction()
-                .setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_right_to_left
-                        , R.anim.enter_left_to_right, R.anim.exit_left_to_right)
-                .replace(R.id.container, fragment)
-                .addToBackStack(null)
-                .commit();
+         if(index == 0) previous.setVisibility(View.INVISIBLE);
     }
 }
